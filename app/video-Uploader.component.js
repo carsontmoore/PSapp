67d23442045b29590155842bad4ff0d40842e78e
carsontmoore <carsontmooore@gmail.com>
2017-10-28 20:48:40 -0500

    Fixes file structure for plugin and script issues

--- app/video-Uploader.component.js
+++ app/video-Uploader.component.js
@@ -3,7 +3,7 @@ angular.
 	component('videoUploader', {
 		template:
 			'<div>' +
-				'<input id="fileUpload" type="file" name="file" data-url="https://upload.wistia.com">' +
+				'<input id="fileupload" type="file" name="files[]" data-url="https://upload.wistia.com">' +
 				'<span id="percent">0%</span>' +
 				'<div id="progressBar">' +
 	  			'<div class="bar" style="width: 0%;"></div>' +
@@ -13,13 +13,17 @@ angular.
 		  $('#fileupload').fileupload({
 		      dataType: 'json',
 		      formData: {
-		      	api_password: '29b0b64ab2e6d803b45d132c933468ac49eb9b14f1e8b43ef8a46028ac2c1cf9'
+		      	api_password: '29b0b64ab2e6d803b45d132c933468ac49eb9b14f1e8b43ef8a46028ac2c1cf9',
+		      	method: 'POST'
 		      },
 		      add: function (e, data) {
+		      	console.log('Log before submit')
 		      	data.submit();
+		      	console.log('AFTER SUBMIT')
 		      },
 		      done: function (e, data) {
 		      	data.context.text('Upload complete.');
+		      	console.log('UPLOAD IS COMPLETE')
 		      },
 		      progressall: function (e, data) {
 		        var progress = parseInt(data.loaded / data.total * 100, 10);
