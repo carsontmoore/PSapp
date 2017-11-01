89c4ad20fb0c6d66ccd66fe440947ff5cb5134ee
carsontmoore <carsontmooore@gmail.com>
2017-11-01 15:30:24 -0500

    Fixes node issue

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 
+app.set('port', (process.env.PORT || 3001));
+
 app.use(express.static(__dirname));
 
-app.listen(3000, function () {
-  console.log('Process St listening on port 3000')
+app.listen(app.get('port'), function () {
+  console.log('Process St listening on port', app.get('port'));
 });
 
 module.exports = app;
